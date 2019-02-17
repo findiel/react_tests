89fe3f85a59f48a204d455526c11e810785f7f8b
Fjollsfinn <btuchowski@fream.pl>
2019-02-17 18:32:47 +0100

    edit readme.md

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
     render() {
         return (
             <div className="footer-flex">
-                <h3>THIS IS <a href="https://github.com/Fjollsfinn">FJOLLSFINN</a> TESTING PAGE. ALL RIGHTS RESERVED.</h3>
+                <h3>THIS IS <a href="https://github.com/Fjollsfinn">FJOLLSFINN</a> TESTING PAGE.</h3>
             </div>
         )
     }
