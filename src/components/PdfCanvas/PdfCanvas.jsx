ecc2502d9c249bfd15b6646152afd700dd0f3552
Bartłomiej <btuchowski@fream.pl>
2020-08-09 22:55:56 +0200

    working dispplay of pdf file

--- src/components/PdfCanvas/PdfCanvas.jsx
+++ src/components/PdfCanvas/PdfCanvas.jsx
@@ -1,57 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import SkywalkerPDF from '../../assets/pdfFile/Skywalker.pdf';
-import { Document, Page, pdfjs } from "react-pdf";
-var pdfjsLib = require("pdfjs-dist");
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import skywalkerPdf from '../../assets/pdfFile/Skywalker.pdf';
 
-
-const useStyles = makeStyles(theme => ({
-  root: {
-  },
-}));
-
-// Setting worker path to worker bundle.
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const PdfCanvas = ({}) => {
-  const classes = useStyles();
-  const canvas = React.createRef();
+function PdfCanvas() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-  React.useEffect(() => {
-    var loadingTask = pdfjs.getDocument( '../../assets/pdfFile/React.js.pdf');
-    if(canvas && canvas.current) {
-        console.log(canvas)
-        loadingTask.promise.then(doc => {
-            doc.getPage(1).then(page => {
-                const context = canvas.current.getContext("2d");
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
-                const viewport = page.getViewport(1);
-                canvas.current.width = viewport.width;
-                canvas.current.height = viewport.height;
-
-                const renderTask = page.render({
-                    canvasContext: context,
-                    viewport: viewport,
-                })
-                return renderTask.promise;
-            })
-        })
-        .catch(function (reason) {
-            console.error("Error: " + reason);
-        });
-    }
-  },[canvas])
-
-  return  (
-    <canvas ref={canvas} id="pdfCanvas"></canvas>
-  )
-};
-
-PdfCanvas.propTypes = {
-};
-
-PdfCanvas.defaultProps = {
-};
-
+  return (
+    <div>
+      <Document
+        file={skywalkerPdf}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>Page {pageNumber} of {numPages}</p>
+    </div>
+  );
+}
 export default PdfCanvas;
